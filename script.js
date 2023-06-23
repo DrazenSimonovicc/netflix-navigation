1c252adfe08755113e98644b14ae05b3faf5f060
Drazen Simonovic <drazensimonovic18@gmail.com>
2023-06-23 14:30:35 +0200

    netflix

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const nav = document.querySelectorAll(".nav");
+
+openBtn.addEventListener("click", () => {
+  nav.forEach((nav_el) => nav_el.classList.add("visible"));
+});
+
+closeBtn.addEventListener("click", () => {
+  nav.forEach((navEl) => navEl.classList.remove("visible"));
+});
